ceffddae74db277e61ede2020c7784a80823b102
Michael Ho <michaelhochum@gmail.com>
2015-02-02 13:40:43 -0500

    Added cors

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ var UserModel    = require('./libs/mongoose').UserModel;
 var sudoku = require('sudoku-c');
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
